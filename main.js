cf8fef598e896cb6a1d6b9513b2a1705637bf344
swapnillps0105 <114773664+swapnillps0105@users.noreply.github.com>
2025-05-26 16:42:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+      strings: ['Data Engineer.', 'Full Stack developer.','Web Developer.','ETL Developer'],
+      typeSpeed: 100,
+      backSpeed:100,
+      backDelay:1000,
+      loop:true
+    });
